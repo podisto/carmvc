512064ceb7258f0589ac1a5bee6d4f9dab4ca6b6
podisto <podisto>
2017-11-15 09:24:28 +0000

    affichage des détails.

--- src/main/java/controllers/SearchController.java
+++ src/main/java/controllers/SearchController.java
@@ -1,14 +1,14 @@
 package controllers;
 
 import java.util.List;
+import java.util.Set;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Textbox;
+import org.zkoss.zul.*;
+import org.zkoss.zul.ext.Selectable;
 
 import models.Car;
 import services.CarService;
@@ -19,8 +19,23 @@ public class SearchController extends SelectorComposer<Component> {
 	@Wire
 	private Textbox keywordBox;
 	
-	@Wire("#carListBox")
+	@Wire
 	private Listbox carListBox;
+	
+	@Wire
+	private Label modelLabel;
+	
+	@Wire
+	private Label makeLabel;
+	
+	@Wire
+	private Label priceLabel;
+	
+	@Wire
+	private Label descriptionLabel;
+	
+	@Wire
+	private Image previewImage;
 	
 	private CarService carService = new CarServiceImpl();
 	
@@ -28,7 +43,19 @@ public class SearchController extends SelectorComposer<Component> {
 	public void search() {
 		String keyword = keywordBox.getValue();
 		List<Car> results = carService.search(keyword);
-		System.out.println(results.size());
 		carListBox.setModel(new ListModelList<Car>(results));
+	}
+	
+	@Listen("onSelect = #carListBox")
+	public void showDetail() {
+		Set<Car> selection = ((Selectable<Car>)carListBox.getModel()).getSelection();
+		if (selection != null && !selection.isEmpty()) {
+			Car selected = selection.iterator().next();
+			previewImage.setSrc(selected.getPreview());
+			modelLabel.setValue(selected.getModel());
+			makeLabel.setValue(selected.getMake());
+			priceLabel.setValue(selected.getPrice().toString());
+			descriptionLabel.setValue(selected.getDescription());
+		}
 	}
 }
